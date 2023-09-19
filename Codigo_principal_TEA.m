b604fe179bda7edb42c73c74417979f09d8e8261
mariavmuniz <soaresmunizm@gmail.com>
2023-09-19 15:57:01 -0300

    chore(Codigo_principal_TEA): parameterizing variables

--- Codigo_principal_TEA.m
+++ Codigo_principal_TEA.m
@@ -25,37 +25,26 @@
     pkg load signal
     
 % Carregar os sinais salvos em uma pasta
-    %myFolder = '/home/mavi/Documentos/Projects/attribute-extraction/ASD_FINAL/'; %pasta com sinais
-    myFolder = '/home/mavi/Documentos/Projects/attribute-extraction/P_FINAL/'; %pasta com sinais
+    myFolder = ' '; %pasta com sinais em .mat 
     addpath(myFolder)
     files = dir(myFolder); %files contem os nomes dos arquivos que estao na pasta
-    pathSave = '/home/mavi/Documentos/Projects/attribute-extraction/'; %pasta para salvar arff
+    pathSave = ' '; %pasta para salvar o arff
 
 % Parametros iniciais 
-    Fs = 512;            % Frequencia de amostragem em Hz
+    Fs = 512;           % Frequencia de amostragem em Hz
     janela=2;           % Tamanho da janela em segundos
-    superposicao=0.5;      % Superposicao da janela em segundos
-    n_canais = 64;       % Numero de canais (eletrodos)
-    %classes = [0]; 
-    classes = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ];
-    %classes = [0 0 1 1];     % A ordem das classes deve corresponder � ordem dos arquivos salvos em files
-    atributos_total =[]; % Criacao de vetores
+    superposicao=0.5;   % Superposicao da janela em segundos
+    n_canais = 64;      % Numero de canais (eletrodos)
+    classes = [0];
+    %classes = [0 0 1 1]; % A ordem das classes deve corresponder � ordem dos arquivos salvos em files
+    atributos_total =[];  % Criacao de vetores
     vetor_classes =[];
 
   for i = 3:length(files)   
     
     [Y] = load(files(i).name); % Carrega o sinal e salva na variavel X
      Y = [struct2cell(Y){:}];
-     
-    % Exemplo de Projeto de filtros passa-banda: 
-      % Exemplo: Filtrar o sinal para retirar os efeitos do EOG    
-      %freq_low = 8;  %Frequencia em Hz
-      %freq_high = 30;   
-      %order = 6;      %Ordem do filtro
-      %f_low = 2*freq_low/Fs;
-      %f_high = 2*freq_high/Fs;
-      %[b,a] = butter(order, [f_low f_high]); %Filtro de butterworth passa-banda
-    
+
     % Janelamento do sinal e Extracao de atributos
       atributos = [];
       [atributos] = extracao_atributos(Y, Fs, janela, superposicao, n_canais);
@@ -77,4 +66,3 @@
   % Gerar arquivo ARFF e CSV para posterior classificacao
       matriz = [atributos_total vetor_classes];
       [ARFF] = geraARFF(pathSave,'P',matriz);
-      csvwrite(strcat(pathSave,'.csv'),matriz);
